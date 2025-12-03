863d2464fb4af4175c9000d1c4f3fa87a8e843c0
OlegZaitsev71 <oleg.zaitsew@gmail.com>
2025-12-03 20:03:59 +0300

    DB Redis, commands

--- hw9_zoa_redis.py
+++ hw9_zoa_redis.py
@@ -14,6 +14,14 @@ def setvar(variable_name, variable_value):
     my_server = redis.Redis(connection_pool=POOL)
     my_server.set(variable_name, variable_value)
 
+def decrvar(variable_name, num) ->bool:
+    if isinstance(num, int):
+        my_server = redis.Redis(connection_pool=POOL)
+        my_server.decr(variable_name, num)
+        return True
+    else:
+        return False
+
 # test
 #setvar('zoa_key1', 'zoa_key1_val')
 #print(getvar('zoa_key1'))
